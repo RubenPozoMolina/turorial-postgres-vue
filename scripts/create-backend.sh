422696bd3fc9ccc67d80b0176cbe90ceba6d73f7
Rubén Pozo <rubenpozomolina@gmail.com>
2019-12-20 08:24:46 +0100

    Ignore backend-project and added required packages to script

--- scripts/create-backend.sh
+++ scripts/create-backend.sh
@@ -3,7 +3,7 @@ rm -rf backend-project
 mkdir backend-project
 cd backend-project
 npm init -y
-npm i typeorm ts-node --save
+npm i typeorm ts-node typescript pg --save
 ./node_modules/.bin/ts-node ./node_modules/.bin/typeorm init --express --db postgres
 ./node_modules/.bin/ts-node ./node_modules/.bin/typeorm schema:sync
 npm i
